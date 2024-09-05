f08a5ad520c574df45fec0fa6f9447a99791d90e
Matthew Fox <matthew.nutter@turntown.digital>
2024-09-05 10:47:48 +0100

    demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import WebFont from 'webfontloader';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'font-loader';
+  loadFontViaFontFace() {
+    const font = new FontFace("Grey Qo", "url(https://fonts.gstatic.com/s/greyqo/v9/BXRrvF_Nmv_TyXxNPOxa9EX3UcA.woff2)", {
+      style: "normal",
+      weight: "400",
+    });
+
+    document.fonts.add(font);
+  }
+
+  loadFontViaFontLoader() {
+    WebFont.load({google: {families: ['Nerko One']}})
+  }
 }
